0ebbe99d9cdbbbc033f31577c48547ed21889d38
Jan Behrens <dev@jpbehrens.de>
2021-09-23 14:50:41 +0200

    add randomNumber

---
+++ randomNumber/randomNumber.js
@@ -0,0 +1,59 @@
+/**
+ * JP BEHRENS - RandomNumber
+ */
+function randomNumber({ quanty = 1, sort = null, uniqe = true, min = 0, max = 99 }) {
+
+    let eCode;
+
+    if (quanty > (max - min)) {
+        eCode = 101; // Error 101: angeforderte Anzahl größer als die differenz zwischen min und max Wert
+    } else {
+
+        const numbs = [[],[]];   // Lerrer Array in den die zufälligen Nummern gespeichert werden
+
+        let runs = 0; // Hier werden die benötighten durchläufe gezählt
+
+        while (numbs[0].length < quanty) {
+            // Erzrugt eine zufällige Zahl von Min bis Max
+            const randNum = Math.floor(Math.random() * (max - min) + min);
+            // Wenn die nummern uniqe sein sollen
+            if (uniqe) {
+                if (!numbs[0].includes(randNum)) {
+                    numbs[0].push(randNum);
+                }
+            } else {
+                numbs[0].push(randNum);
+            }
+
+            // Durchläufe werden hochgezäghlt
+            runs++;
+        }
+
+
+        for(let i = min; i<max;i++){
+            if (!numbs[0].includes(i)) {
+                numbs[1].push(i);
+            }
+
+        }
+
+        // Sortieren
+        if (sort === 'up') { numbs.sort((a, b) => a - b); } // Aufsteigend
+        if (sort === 'down') { numbs.sort((a, b) => b - a); } // Absteigend
+
+        return {
+            error: false,
+            numbers: {
+                selected: numbs[0],
+                notSelected: numbs[1]
+            },
+            runs
+        }
+    }
+
+    return {
+        error: true,
+        eCode: 101
+    }
+    //return numbs;
+}
---
+++ randomNumber/readme.md
@@ -0,0 +1,24 @@
+# randomNumber
+With this function the user can generate random numbers. The number range is freely selectable. Furthermore, the output can be controlled via arguments. 
+
+## Arguments (optinal)
+- min : int
+- max : int 
+- quanty: int
+- sort: string (up|down);
+- uniqe: bool (false|true); 
+
+## Arguments default
+- min : 0
+- max : 99 
+- quanty: 1
+- sort: null;
+- uniqe: true; 
+
+## Example Execute
+```` 
+randomNum({ quanty: 33, sort:'up', uniqe: true, max:50 });
+```` 
+
+
+
